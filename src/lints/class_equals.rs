12da46d1754c20e0b8128d8cab04305b93e4a806
etiennebacher <etienne.bacher@protonmail.com>
2025-01-14 19:41:15 +0100

    more on class_equals

--- src/lints/class_equals.rs
+++ src/lints/class_equals.rs
@@ -12,11 +12,8 @@ impl LintChecker for ClassEquals {
     fn check(&self, ast: &RSyntaxNode, loc_new_lines: &[usize], file: &str) -> Vec<Message> {
         let mut messages = vec![];
         let bin_expr = RBinaryExpression::cast(ast.clone());
-        if bin_expr.is_none() {
-            return messages;
-        }
 
-        if node_is_in_square_brackets(ast) {
+        if bin_expr.is_none() || node_is_in_square_brackets(ast) {
             return messages;
         }
 
@@ -32,19 +29,38 @@ impl LintChecker for ClassEquals {
         let lhs = children.next().unwrap();
         let rhs = children.next().unwrap();
 
-        if let Some(fun) = lhs.first_child() {
-            if fun.text_trimmed() != "class" {
-                return messages;
-            }
+        let left_is_class = match lhs.first_child() {
+            Some(x) => x.text_trimmed() == "class",
+            None => false,
+        };
+        let right_is_class = match rhs.first_child() {
+            Some(x) => x.text_trimmed() == "class",
+            None => false,
+        };
+        let left_is_string = lhs.kind() == RSyntaxKind::R_STRING_VALUE;
+        let right_is_string = rhs.kind() == RSyntaxKind::R_STRING_VALUE;
+
+        if (!left_is_class && !right_is_class) || (!left_is_string && !right_is_string) {
+            return messages;
+        }
+
+        let fun_name;
+        if operator.kind() == RSyntaxKind::EQUAL2 {
+            fun_name = "inherits";
         } else {
-            return messages;
-        }
+            fun_name = "!inherits"
+        };
 
-        if rhs.kind() != RSyntaxKind::R_STRING_VALUE {
-            return messages;
-        }
+        let fun_content;
+        let class_name;
 
-        let fun_content = get_args(&lhs).and_then(|x| Some(x.text_trimmed()));
+        if left_is_class {
+            fun_content = get_args(&lhs).and_then(|x| Some(x.text_trimmed()));
+            class_name = rhs.text_trimmed();
+        } else {
+            fun_content = get_args(&rhs).and_then(|x| Some(x.text_trimmed()));
+            class_name = lhs.text_trimmed();
+        };
 
         let (row, column) = find_row_col(ast, loc_new_lines);
         let range = ast.text_trimmed_range();
@@ -52,7 +68,7 @@ impl LintChecker for ClassEquals {
             filename: file.into(),
             location: Location { row, column },
             fix: Fix {
-                content: format!("inherits({}, {})", fun_content.unwrap(), rhs.text_trimmed()),
+                content: format!("{}({}, {})", fun_name, fun_content.unwrap(), class_name),
                 start: range.start().into(),
                 end: range.end().into(),
             },
